69b73aab594682882cb34750561f44f4b7aed159
Yegor Bugayenko <yegor@tpc2.com>
2013-10-13 16:40:50 +0200

    change classifier only if needed

--- src/main/java/com/jcabi/mysql/maven/plugin/ClassifyMojo.java
+++ src/main/java/com/jcabi/mysql/maven/plugin/ClassifyMojo.java
@@ -88,7 +88,10 @@ public final class ClassifyMojo extends AbstractMojo {
         );
         final String existing = this.project.getProperties()
             .getProperty(this.classifier);
-        if (existing != null && !existing.equals(value)) {
+        if (existing == null) {
+            this.project.getProperties().setProperty(this.classifier, value);
+            Logger.info(this, "${%s} set to \"%s\"", this.classifier, value);
+        } else if (!existing.equals(value)) {
             throw new MojoFailureException(
                 String.format(
                     // @checkstyle LineLength (1 line)
@@ -97,8 +100,6 @@ public final class ClassifyMojo extends AbstractMojo {
                 )
             );
         }
-        this.project.getProperties().setProperty(this.classifier, value);
-        Logger.info(this, "${%s} set to \"%s\"", this.classifier, value);
     }
 
     /**
